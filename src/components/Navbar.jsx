8589c7c325fa258fc7a270a29c205ac7c162682a
Gillian Markley <gillianmarkley@gmail.com>
2022-09-19 18:54:28 -0700

    some navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import logo from '.././gd-logo.jpg';
+import { colors } from "../styling";
 
-function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/rsvp">RSVP</Link>
-        </li>
-        <li>
-          <Link to="/photos">Photos</Link>
-        </li>
-        <li>
-          <Link to="/gifts">Gifts</Link>
-        </li>
-        <li>
-          <Link to="/details">Details</Link>
-        </li>
-        <li>
-          <Link to="/weddingparty">Wedding Party</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+   Navbar,
+    NavItem,
+    NavbarToggler,
+    Collapse,
+    NavLink,
+    Nav,
+} from 'reactstrap';
+
+function NavbarComponent() {
+   const [isOpen, setIsOpen] = React.useState(false);
+  
+   return (
+       <div style={{
+           display: 'block', width: '100%', padding: 30, backgroundColor: colors.greyWhite
+       }}>
+           <Navbar style={{ backgroundColor: colors.lightBlue }} expand="md">
+           <img src={logo} className="App-logo" alt="logo" width="10%" height="10%" />
+               <NavbarToggler onClick={() => { setIsOpen(!isOpen) }} />
+               <Collapse isOpen={isOpen} navbar>
+                   <Nav className="mr-auto" navbar>
+                   <NavItem>
+                          <NavLink href="/">Home</NavLink>
+                      </NavItem>
+                      <NavItem>
+                          <NavLink href="/rsvp">RSVP</NavLink>
+                      </NavItem>
+                      <NavItem>
+                          <NavLink href="/photos">Photos</NavLink>
+                      </NavItem>
+                      <NavItem>
+                          <NavLink href="/gifts">Gifts</NavLink>
+                      </NavItem>
+
+                      <NavItem>
+                          <NavLink href="/details">Details</NavLink>
+                      </NavItem>
+                      <NavItem>
+                          <NavLink href="/weddingparty">Wedding Party</NavLink>
+                      </NavItem>
+                   </Nav>
+               </Collapse>
+           </Navbar>
+   
+       </div >)
 }
 
-export default Navbar;
+export default NavbarComponent;
---
+++ src/styling.js
@@ -0,0 +1,12 @@
+export const colors = {
+    greyWhite : '#e9e9eb', 
+    lightBlue :"#afd1e1", 
+    navyBlue : "#18293c",
+    turquoise : "#016382",
+    lightTurquoise : "#467f96",
+    lightGreen : "#b7bea3",
+    mediumGreen : "#828c5a",
+    darkGreen : "#5a6731",
+    lightBeige : "#dad3cb",
+    Beige : "#c2a484", 
+}
