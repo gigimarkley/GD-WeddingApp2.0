5bd39676d09c8fbbf63f468da4885682b1fac1bb
Gillian Markley <gillianmarkley@gmail.com>
2022-09-21 17:31:00 -0700

    got rid of a random footer

--- src/pages/OurStory.jsx
+++ src/pages/OurStory.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {pageTitle, pageContainers} from '../styling'
 import timelineImage from '../images/timeline.png'
-import Footer from '../components/Footer';
 
 function OurStory() {
     return (
         <div style={pageContainers}>
             <h1 style={pageTitle}>Our Story</h1>
             <img src={timelineImage}  alt="logo" width="50%"/>
-            <Footer/>
         </div>
     );
 }
